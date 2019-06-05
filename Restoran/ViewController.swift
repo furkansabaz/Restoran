2ae67902159f860fc0356e8725f95539dd7701dd
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-05 21:45:04 +0300

    Initial Commit

---
+++ Restoran/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restoran
+//
+//  Created by Furkan Sabaz on 5.06.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
