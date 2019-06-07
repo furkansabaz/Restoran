d7ad1c059700f29df648c94dfcda1618551c8386
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-06-07 18:18:55 +0300

    Ders 13- Moya ve AlamofireImage Yükleme

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '11.0'
+
+target 'Restoran' do
+  use_frameworks!
+
+  pod 'Moya'
+  pod 'AlamofireImage'
+
+end
---
+++ Pods/Target Support Files/Pods-Restoran/Pods-Restoran-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Restoran : NSObject
+@end
+@implementation PodsDummy_Pods_Restoran
+@end
